1d4de0cbe17e3806816ff8ae78823d70f07b2baf
teru <nicori.jsb@gmail.com>
2019-12-22 04:09:24 +0900

    マスクで角丸をつける

--- CornerRoundedImageView/Classes/CornerRoundedImageView.swift
+++ CornerRoundedImageView/Classes/CornerRoundedImageView.swift
@@ -1,6 +1,14 @@
 import UIKit
 
 class CornerRoundedImageViewImageView: UIImageView {
+  private let maskLayer = CAShapeLayer()
+  
+  @IBInspectable var cornerRadius: CGFloat = 8 {
+    didSet {
+      setupCornerRoundedMask()
+    }
+  }
+  
   override init(frame: CGRect) {
     super.init(frame: frame)
     setup()
@@ -21,5 +29,15 @@ class CornerRoundedImageViewImageView: UIImageView {
     setup()
   }
   
-  private func setup() {}
+  private func setup() {
+    setupCornerRoundedMask()
+  }
+  
+  private func setupCornerRoundedMask() {
+    let path = UIBezierPath(roundedRect: CGRect(origin: CGPoint(x: (bounds.width - frame.size.width) / 2, y: (bounds.height - frame.size.height) / 2), size: frame.size), cornerRadius: cornerRadius)
+    maskLayer.path = path.cgPath
+    maskLayer.frame = bounds
+    maskLayer.fillRule = .evenOdd
+    layer.mask = maskLayer
+  }
 }

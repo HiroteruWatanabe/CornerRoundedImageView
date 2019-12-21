6ac68013178e995529b45e65f4638a0680068678
teru <nicori.jsb@gmail.com>
2019-12-22 03:37:41 +0900

    イニシャライザーを実装

--- CornerRoundedImageView/Classes/CornerRoundedImageView.swift
+++ CornerRoundedImageView/Classes/CornerRoundedImageView.swift
@@ -1,1 +1,25 @@
+import UIKit
 
+class CornerRoundedImageViewImageView: UIImageView {
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setup()
+  }
+  
+  override init(image: UIImage?) {
+    super.init(image: image)
+    setup()
+  }
+  
+  override init(image: UIImage?, highlightedImage: UIImage?) {
+    super.init(image: image, highlightedImage: highlightedImage)
+    setup()
+  }
+  
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    setup()
+  }
+  
+  private func setup() {}
+}
